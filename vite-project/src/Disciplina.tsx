0c8e9653626952b7f05451ce8713a95a60ce7861
Sarah Cristina <sarahsilva34567@gmail.com>
2024-11-11 20:37:26 -0300

    Adiciona Estilo

--- vite-project/src/Disciplina.tsx
+++ vite-project/src/Disciplina.tsx
@@ -3,6 +3,7 @@ import "./Disciplina.css";
 function Disciplina (){
     return (
     <div className="disciplina"> 
+    
             <div>DCC207</div>
             <div>Desenvolvimento Web Front-End</div>
         </div>
