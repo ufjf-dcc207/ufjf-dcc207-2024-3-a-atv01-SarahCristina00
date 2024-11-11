98402a30230039ad7c49e5cb0363c46d8ee85456
Sarah Cristina <sarahsilva34567@gmail.com>
2024-11-11 19:42:24 -0300

    adiciona titulo pages

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
+
 export default defineConfig({
+  base:"ufjf-dcc207-2024-3-a-atv01-SarahCristina00",
   plugins: [react()],
 })
